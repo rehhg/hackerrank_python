94a80b4882364272f5c467fb9a484193ae390e72
rehhg <rehhg@mail.ua>
2019-01-02 20:55:30 +0200

    add tasks new year chaos and quicksort in-place

---
+++ new_year_chaos/solution.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+
+# Complete the minimumBribes function below.
+
+
+def minimumBribes(finalLine):
+    if invalid(finalLine):
+        return "Too chaotic"
+    return bubbleSort(finalLine)
+
+
+def invalid(finalLine):
+    return any(didBribeMoreThanTwoPeople(person, index)
+               for index, person in enumerate(finalLine))
+
+
+def didBribeMoreThanTwoPeople(person, index):
+    return index + 2 < person - 1
+
+
+def bubbleSort(line):
+    swaps = 0
+    swappedInCurrentPass = False
+
+    for person in range(len(line)):
+        for i in range(0, len(line) - 1):
+            if line[i] > line[i + 1]:
+                line[i], line[i + 1] = line[i + 1], line[i]
+                swaps += 1
+                swappedInCurrentPass = True
+        if swappedInCurrentPass:
+            swappedInCurrentPass = False
+        else:
+            break
+
+    return swaps
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        print(minimumBribes(q))
